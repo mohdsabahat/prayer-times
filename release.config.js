bbb7ec0dd97a1e9148a72522559a3deeb3df0199
Mohd Sabahat <mohd.sabahat@comviva.com>
2025-02-16 03:48:51 +0530

    ci(release): update release configuration to include npm and git assets

--- release.config.js
+++ release.config.js
@@ -4,12 +4,24 @@ module.exports = {
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
         "@semantic-release/changelog",
-        "@semantic-release/git",
+        [
+            "@semantic-release/npm",
+            {
+                "npmPublish": false
+            }
+        ],
+        [
+            "@semantic-release/git",
+            {
+                "assets": ["package.json", "package-lock.json", "CHANGELOG.md"],
+                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+            }
+        ],
         [
             "@semantic-release/github",
             {
                 "assets": [
-                    { "path": "dist/**", "label": "Build files" }
+                    { "path": "dist/**" }
                 ]
             }
         ]
